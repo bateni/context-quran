b030c3c620098947a860931b0443e0fd6f327e34
Hossein <bateni@gmail.com>
2016-08-20 19:32:02 -0400

    balance sizes of output files

--- tools/convert.py
+++ tools/convert.py
@@ -4,7 +4,7 @@ input_prefix = 'raw-'         # raw-99.txt
 output_prefix = 'quran-text-' # quran-text-99.tex 
 input_meta_data  = 'raw-meta.txt'
 output_meta_data = 'quran-meta.lua'
-verses_per_chunk = 100
+verses_per_chunk = 50
 
 
 chapter_names = {}
@@ -25,21 +25,28 @@ def read_meta():
 
 # produce output data files and collect verse count info, etc.
 def produce_buffers():
+    global_verse = 0
     for chap in range(1,115):
         print 'Processing chapter', chap
-        f = open(output_prefix + str(chap) + '.tex', 'w')
         ver = 0
         for line in open(input_prefix + str(chap) + '.txt'):
+            chunk = global_verse / verses_per_chunk
+            if (global_verse % verses_per_chunk) == 0:
+                print 'opening new output file for buffers', chunk
+                f = open(output_prefix + str(chunk) + '.tex', 'w')
             line = line.decode('utf-8')
             line = line.strip('\n')
             ver = ver + 1
+            global_verse = global_verse + 1
             if len(line) == 0:
                 print 'error in verse', ver, 'of chapter', chap
             f.write('\\startbuffer[\\q:%d:%d]\n' % (chap, ver))
             f.write(line.encode('utf-8') + '%\n')
             f.write('\\stopbuffer\n')
         verses_in_chapter[chap] = ver
-        f.close()
+#        f.close()
+    print 'Total of', global_verse, 'verses in', chunk+1,'chunks'
+
 
 
 def produce_meta():
@@ -52,7 +59,8 @@ def produce_meta():
             basmalah = 'false'
         f.write('    [%d] = { basmalah = %s, name = "%s", verses = %d, }\n' %
                 (k, basmalah, chapter_names[k].encode('utf-8'), v))
-    f.write('  }\n')
+    f.write('  },\n')
+    f.write('  verses_per_chunk = %d,\n' % (verses_per_chunk))
     f.write('}\n')
     f.close()
 
