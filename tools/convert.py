212ebdbf0d0d155df4bc0fa236516debf4e06459
Hossein <bateni@gmail.com>
2016-08-20 18:48:55 -0400

    basic script to conver to start/stopbuffer format

---
+++ tools/convert.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+
+input_prefix = 'raw-'         # raw-99.txt
+output_prefix = 'quran-text-' # quran-text-99.tex 
+input_meta_data  = 'raw-meta.txt'
+output_meta_data = 'quran-meta.lua'
+verses_per_chunk = 100
+
+# todo: read other meta information, e.g., chapter names
+
+verses_in_chapter = {}
+
+for chap in range(1,115):
+    print 'Processing chapter', chap
+    f = open(output_prefix + str(chap) + '.tex', 'w')
+    ver = 0
+    for line in open(input_prefix + str(chap) + '.txt'):
+        line = line.decode('utf-8')
+        line = line.strip('\n')
+        ver = ver + 1
+        if len(line) == 0:
+            print 'error in verse', ver, 'of chapter', chap
+        f.write('\\startbuffer[\\q:%d:%d]\n' % (chap, ver))
+        f.write(line.encode('utf-8') + '\n')
+        f.write('\\stopbuffer\n')
+    verses_in_chapter[chap] = ver
+    f.close()
+
+print verses_in_chapter
+
