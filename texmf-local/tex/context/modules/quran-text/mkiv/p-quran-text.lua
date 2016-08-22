f9db06be8528a397e0ac3ba8c438511710af4ec0
Hossein <bateni@gmail.com>
2016-08-21 23:25:12 -0400

    namespaces working with different sources or prefixes

--- texmf-local/tex/context/modules/quran-text/mkiv/p-quran-text.lua
+++ texmf-local/tex/context/modules/quran-text/mkiv/p-quran-text.lua
@@ -1,49 +1,67 @@
 if not modules then modules = { } end modules ['p-quran-text'] = {
-    version   = 1.001,
+    version   = 1.010,
     comment   = "companion to p-quran-text.mkiv",
     author    = "Mohammad Hossein Bateni",
     copyright = "Mohammad Hossein Bateni",
     license   = "GNU General Public License v3"
 }
 
-moduledata.qurantext     = moduledata.database     or { }
+moduledata.qurantext           = moduledata.database            or {}
+moduledata.qurantext.instances = moduledata.qurantext.instances or {}
 
 local qurantext = moduledata.qurantext
+local instances = qurantext.instances
 
-local function init(name)
-  qurantext.data = dofile(resolvers.findfile('quran-meta.lua'))
-  qurantext.psum = {}
-  local psum = 0
-  for chap, info in ipairs(qurantext.data.chapters) do
-    psum = psum + info.verses
-    qurantext.psum[chap] = psum
-  end
+-- Lua does not need to know the name of the construct.
+-- It only processes the meta data from the prefix.
+local function init(prefix)
+   if instances[prefix] then
+      return
+   end
+   metafile = resolvers.findfile(prefix .. '-meta.lua')
+   if not metafile or metafile == '' then
+      error('bad prefix ' .. prefix)
+      return
+   end
+   instances[prefix] = dofile(metafile)
+   local instance = instances[prefix]
+
+   instance.psum = {}
+   local psum = 0
+   for chap, info in ipairs(instance.chapters) do
+      psum = psum + info.verses
+      instance.psum[chap] = psum
+   end
 end
 
 local function error(msg)
   print('quran-text error:', msg)
 end
 
--- TODO: add initializer with cache on lua side
-local function loadversechunk(name,chap,ver)
-  local my_chap = qurantext.data.chapters[chap]
-  if not my_chap then
-    error('invalid chapter number ' .. chap)
-    return
-  end
-  if ver < 1 or ver > my_chap.verses then
-    error('invalid verse number ' .. ver .. ' in chapter ' .. chap)
-    return
-  end
-  local global_verse = qurantext.psum[chap - 1] or 0
-  global_verse = global_verse + ver - 1
-  local chunk = math.floor(global_verse / qurantext.data.verses_per_chunk)
-  context.begingroup()
-  context("\\def\\q{quran-text}")
-  context.input('quran-text-' .. chunk .. '.tex')
-  context.endgroup()  
+
+local function loadversechunk(prefix,chap,ver)
+   init(prefix)
+   local instance = instances[prefix]
+   if not instance then
+      return
+   end
+   local my_chap = instance.chapters[chap]
+   if not my_chap then
+      error('invalid chapter number ' .. chap)
+      return
+   end
+   if ver < 1 or ver > my_chap.verses then
+      error('invalid verse number ' .. ver .. ' in chapter ' .. chap)
+      return
+   end
+   local global_verse = instance.psum[chap - 1] or 0
+   global_verse = global_verse + ver - 1
+   local chunk = math.floor(global_verse / instance.verses_per_chunk)
+   context.begingroup()
+   context("\\def\\q{" .. prefix .. "-text}")
+   context.input(prefix .. '-text-' .. chunk .. '.tex')
+   context.endgroup()  
 end
 
 qurantext.loadversechunk = loadversechunk
 
-init('')
